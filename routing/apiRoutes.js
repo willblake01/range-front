c2e5aa34688788de9d000b443836a1227d9ed8ee
William Blake <willblakebooking@gmail.com>
2018-09-11 20:46:28 -0500

    revert

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var db = require("../models");
 module.exports = function (app, passport) {
-    app.get('/api/products', function (req, res) {
+    app.get("/api/products", function (req, res) {
         db.Products.findAll({}).then(function (result) {
             res.json(result);
         });
--- routing/viewRoutes.js
+++ routing/viewRoutes.js
@@ -31,7 +31,7 @@ module.exports = function (app, passport) {
       loggedIn: loggedInView(req)
     });
   });
-  app.get('/products/:category', function (req, res) {
+  app.get("/products/:category", function (req, res) {
     db.Products.findAll({
       where: { category: req.params.category }
 
@@ -50,7 +50,7 @@ module.exports = function (app, passport) {
     });
   });
 
-  app.get('/products/:category/:id', function (req, res) {
+  app.get("/products/:category/:id", function (req, res) {
     db.Products.findById(
       req.params.id
     ).then(function (results) {
@@ -118,7 +118,7 @@ module.exports = function (app, passport) {
 function isLoggedIn(req, res, next) {
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated()) return next();
-  // if they aren't redirect them to the home page
+  // if they aren"t redirect them to the home page
   res.redirect("/");
 }
 
