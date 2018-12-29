0b37bd8695c9063d7d2a5adcd6392d60f7b20b9c
William Blake <willblakebooking@gmail.com>
2018-12-29 17:20:17 -0600

    add yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,11 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: '.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-server.express.use(cookieParser());
-
-// Decode the JWT so we can get the user ID on each request
-server.express.use((req, res, next) => {
-  const {token} = req.cookies;
-  if (token) {
-    const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    // Put the userId onto the req for future requests to access
-    req.userId = userId;
-  }
-  next();
-});
-
-// Create a middleware that populates the user on each request
-server.express.use(async (req, res, next) => {
-// If they aren't logged in, skip this
-if(!req.userId) return next();
-const user = await db.query.user({
-  where: { id: req.userId } },
-  '{ id, permissions, email, name }'
-  );
-  req.user = user;
-  next();
-});
+// TODO Use express middlware to handle cookies (JWT)
+// TODO Use express middlware to populate current user
 
 server.start(
   {
