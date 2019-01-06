c9963c579719603d30513b8d68540ff2a26131d7
William Blake <willblakebooking@gmail.com>
2019-01-06 11:31:25 -0600

    permissions management

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -12,6 +13,16 @@ const Query = {
     return ctx.db.query.user({
       where: { id: ctx.request.userId }
     }, info);
+  },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they're logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 2. Check if the user has permissions to query
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 3. If they do, query all the users
+    return ctx.db.query.users({}, info);
   },
 };
 
---
+++ frontend/.next/static/webpack/static/development/pages/permissions.js.091b86e647017358ad6c.hot-update.js
@@ -0,0 +1,77 @@
+webpackHotUpdate("static/development/pages/permissions.js",{
+
+/***/ "./components/Permissions.js":
+/*!***********************************!*\
+  !*** ./components/Permissions.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_apollo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-apollo */ "./node_modules/react-apollo/react-apollo.browser.umd.js");
+/* harmony import */ var react_apollo__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_apollo__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _ErrorMessage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ErrorMessage */ "./components/ErrorMessage.js");
+var _jsxFileName = "/Users/williamblake/Desktop/range-front/frontend/components/Permissions.js";
+
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  query ALL_USERS_QUERY\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+
+
+
+var ALL_USERS_QUERY = react_apollo__WEBPACK_IMPORTED_MODULE_1___default()(_templateObject());
+
+var Permissions = function Permissions(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_apollo__WEBPACK_IMPORTED_MODULE_1__["Query"], {
+    query: ALL_USERS_QUERY,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, function (_ref) {
+    var data = _ref.data,
+        loading = _ref.loading,
+        error = _ref.error;
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 12
+      },
+      __self: this
+    }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_ErrorMessage__WEBPACK_IMPORTED_MODULE_2__["default"], {
+      error: error,
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 13
+      },
+      __self: this
+    }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 14
+      },
+      __self: this
+    }, "Hey"));
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Permissions);
+
+/***/ })
+
+})
+//# sourceMappingURL=permissions.js.091b86e647017358ad6c.hot-update.js.map
