5d19929800a60c1fd48828b3f36cd4fc6fd2015a
William Blake <willblakebooking@gmail.com>
2019-01-07 15:47:10 -0600

    removing cart items

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "915559f7f19ff21c6369";
+/******/ 	var hotCurrentHash = "c580ea5ee6ed51efa804";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
