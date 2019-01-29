f8a20c746b8175ec690f4f28f1b8e146f993c90f
William Blake <willblakebooking@gmail.com>
2019-01-29 11:53:22 -0600

    prod url

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "04c4e8689b1fcbb426a7";
+/******/ 	var hotCurrentHash = "cce5420325d09f73ac41";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
