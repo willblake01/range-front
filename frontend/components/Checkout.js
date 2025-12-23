ee28074953f3fb2895e0d27b84fe773ceb5ede33
William Blake <willblakebooking@gmail.com>
2025-12-22 18:20:10 -0600

    Update checkout flow

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -12,11 +12,80 @@ import nProgress from 'nprogress';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
 import { useRouter } from 'next/dist/client/router';
-import { useCart } from '../lib/cartState';
 import { formatMoney } from '../lib/formatMoney';
 import { calcTotalPrice } from '../lib/calcTotalPrice';
 import { LargeButton } from '../components';
 import { CURRENT_USER_QUERY, useUser } from '.';
+
+const TotalPrice = styled.p`
+  margin-bottom: 1rem;
+`;
+
+const OrderStyles = styled.div`
+  padding: 20px;
+  position: relative;
+  background: white;
+  height: 100%;
+  width: 100%;
+  min-width: 500px;
+  transform: translateX(100%);
+  transition: all 0.3s;
+  z-index: 5;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  transform: translateX(0);
+  header {
+    border-bottom: 5px solid var(--black);
+    margin-bottom: 2rem;
+    padding-bottom: 2rem;
+  }
+  footer {
+    border-top: 10px double var(--black);
+    margin-top: 2rem;
+    padding-top: 2rem;
+    display: grid;
+    grid-template-columns: auto auto; */
+    align-items: center;
+    font-size: 3rem;
+    font-weight: 900;
+    p {
+      margin: 0 0 1rem 0;
+    }
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    overflow: scroll;
+  }
+`;
+
+  const CheckoutFormStyles = styled.form`
+    box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
+    border: 1px solid rgba(0, 0, 0, 0.06);
+    border-radius: 5px;
+    padding: 1.4rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    min-height: 168px;
+    
+    button {
+      margin-top: auto;
+    }
+  `;
+
+const TestCardInfo = styled.div`
+  font-size: 1.2rem;
+  color: var(--grey);
+  background: var(--offWhite);
+  padding: 0.8rem;
+  border-radius: 3px;
+  
+  strong {
+    color: var(--black);
+  }
+`;
 
 const CREATE_ORDER_MUTATION = gql`
   mutation CREATE_ORDER_MUTATION($token: String!) {
@@ -72,76 +141,6 @@ const CartItem = ({ cartItem }) => {
   );
 };
 
-const TotalPrice = styled.p`
-  margin-bottom: 1rem;
-`;
-
-const OrderStyles = styled.div`
-  padding: 20px;
-  position: relative;
-  background: white;
-  height: 100%;
-  width: 100%;
-  min-width: 500px;
-  transform: translateX(100%);
-  transition: all 0.3s;
-  z-index: 5;
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  transform: translateX(0);
-  header {
-    border-bottom: 5px solid var(--black);
-    margin-bottom: 2rem;
-    padding-bottom: 2rem;
-  }
-  footer {
-    border-top: 10px double var(--black);
-    margin-top: 2rem;
-    padding-top: 2rem;
-    display: grid;
-    grid-template-columns: auto auto; */
-    align-items: center;
-    font-size: 3rem;
-    font-weight: 900;
-    p {
-      margin: 0 0 1rem 0;
-    }
-  }
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    overflow: scroll;
-  }
-`;
-
-  const CheckoutFormStyles = styled.form`
-  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
-  border: 1px solid rgba(0, 0, 0, 0.06);
-  border-radius: 5px;
-  padding: 1.4rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  min-height: 168px;
-  
-  button {
-    margin-top: auto;
-  }
-`;
-
-const TestCardInfo = styled.div`
-    font-size: 1.2rem;
-    color: var(--grey);
-    background: var(--offWhite);
-    padding: 0.8rem;
-    border-radius: 3px;
-    
-    strong {
-      color: var(--black);
-    }
-  `;
-
 const CheckoutForm = () => {
   const user = useUser();
   const [error, setError] = useState();
@@ -149,7 +148,6 @@ const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const router = useRouter();
-  const { closeCart } = useCart();
   const [checkout, { error: graphQLError }] = useMutation(
     CREATE_ORDER_MUTATION,
     {
@@ -198,9 +196,6 @@ const CheckoutForm = () => {
         },
       });
 
-      // 7. Close the cart
-      closeCart();
-
       // 8. turn the loader off
       setLoading(false);
       nProgress.done();
@@ -212,11 +207,13 @@ const CheckoutForm = () => {
     };
   };
 
+  const orderHeader = user ? `${user?.firstName} ${user?.lastName}'s order` : null;
+
   return (
     <>
       <OrderStyles>
         <header>
-          <h1>{user?.firstName} {user?.lastName}'s order</h1>
+          <h1>{orderHeader}</h1>
         </header>
         <ul>
           {user?.cart.map((cartItem) => (
@@ -237,7 +234,7 @@ const CheckoutForm = () => {
         <Link
           href='/order-confirmation'
         >
-          <LargeButton buttonText='Place Order' />
+          <LargeButton type='submit' buttonText='Place Order' />
         </Link>
       </CheckoutFormStyles>
     </>
