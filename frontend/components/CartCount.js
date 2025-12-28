07cd5dd0aa30b44582862e20406e7cf66ea070bc
William Blake <willblakebooking@gmail.com>
2025-12-27 23:34:39 -0600

    Change cart count color to green

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -2,7 +2,7 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import styled from 'styled-components';
 
 const StyledDot = styled.div`
-  background: var(--red);
+  background: var(--green);
   color: white;
   border-radius: 50%;
   padding: 0.5rem;
@@ -15,23 +15,28 @@ const StyledDot = styled.div`
 
 const StyledAnimation = styled.span`
   position: relative;
+
   .count {
     display: block;
     position: relative;
     transition: transform 0.4s;
     backface-visibility: hidden;
   }
+
   .count-enter {
     transform: scale(4) rotateX(0.5turn);
   }
+
   .count-enter-active {
     transform: rotateX(0);
   }
+
   .count-exit {
     top: 0;
     position: absolute;
     transform: rotateX(0);
   }
+
   .count-exit-active {
     transform: scale(4) rotateX(0.5turn);
   }
