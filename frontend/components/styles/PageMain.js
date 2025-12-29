e13da8fde8c0c14249dd5ff9712eeb5ef649df83
William Blake <willblakebooking@gmail.com>
2025-12-28 23:16:12 -0600

    Fix admin layout in logged out state

--- frontend/components/styles/PageMain.js
+++ frontend/components/styles/PageMain.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const PageMain = styled.main`
-  flex: 1;
-  width: 100%;
   display: flex;
   flex-direction: column;
+  flex: 1;
+  width: 100%;
 `;
 
 export { PageMain };
