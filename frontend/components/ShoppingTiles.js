97f24ea426f39ebd28fed4fd7c53ab42439d34bd
William Blake <willblakebooking@gmail.com>
2025-12-29 21:39:32 -0600

    Refactor shopping tiles

--- frontend/components/ShoppingTiles.js
+++ frontend/components/ShoppingTiles.js
@@ -4,53 +4,80 @@ import styled from 'styled-components';
 const StyledTiles = styled.div`
   display: flex;
   flex-wrap: wrap;
-  background-color: var(--black);
   width: 100%;
+
   .tile {
+    position: relative;
     flex: 1;
     min-width: calc(100% / 3);
     height: 48rem;
+
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
-    color: var(--white);
-    font-size: 2.5rem;
+
+    color: var(--offWhite);
+    font-size: 1.5rem;
     text-decoration: none;
-    opacity: 0.42;
-    :hover {
-      opacity: 0.2;
+    overflow: hidden;
+
+    /* overlay tint */
+    &::before {
+      content: '';
+      position: absolute;
+      inset: 0;
+      background: rgba(0, 0, 0, 0.55);
+      transition: background 0.3s ease;
+      z-index: 1;
+    }
+
+    &:hover::before {
+      background: rgba(0, 0, 0, 0.7);
+    }
+
+    span {
+      position: relative;
+      z-index: 2;
+      font-weight: 700;
+      letter-spacing: 0.05em;
+      text-transform: uppercase;
     }
   }
+
   #tents-tile {
-    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509890/range-front/shop_tents.jpg') bottom center no-repeat;
-    background-size: cover;
+    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509890/range-front/shop_tents.jpg')
+      bottom center / cover no-repeat;
   }
+
   #bags-tile {
-    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509889/range-front/shop_sleepingBags.jpg') bottom center no-repeat;
-    background-size: cover;
+    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509889/range-front/shop_sleepingBags.jpg')
+      bottom center / cover no-repeat;
   }
+
   #packs-tile {
-    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509889/range-front/shop_backpacks.jpg') bottom center no-repeat;
-    background-size: cover;
+    background: url('https://res.cloudinary.com/willblake01/image/upload/v1538509889/range-front/shop_backpacks.jpg')
+      bottom center / cover no-repeat;
   }
 `;
 
 const ShoppingTiles = () => (
   <StyledTiles>
-    <Link href='/products/tents'>
-      <div className='tile' id='tents-tile'>
-        Shop Tents
+    <Link href="/products/tents">
+      <div className="tile" id="tents-tile">
+        <span>Shop Tents</span>
       </div>
     </Link>
-    <Link href='/products/backpacks'>
-      <div className='tile' id='packs-tile'>
-        Shop Backpacks
+
+    <Link href="/products/backpacks">
+      <div className="tile" id="packs-tile">
+        <span>Shop Backpacks</span>
       </div>
     </Link>
-    <Link href='/products/sleeping-bags'>
-      <div className='tile' id='bags-tile'>
-        Shop Sleeping Bags
+
+    <Link href="/products/sleeping-bags">
+      <div className="tile" id="bags-tile">
+        <span>Shop Sleeping Bags</span>
       </div>
     </Link>
   </StyledTiles>
