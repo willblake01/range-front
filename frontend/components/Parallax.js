03fc8aa4ffd2dd24baec2bf0f92917cf6a7a0b76
William Blake <willblakebooking@gmail.com>
2026-04-22 15:20:20 -0500

    Cover parallax background image

--- frontend/components/Parallax.js
+++ frontend/components/Parallax.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const StyledParallax = styled.div`
-    background-size: cover;
     background: url('https://res.cloudinary.com/willblake01/image/upload/q_auto/f_auto/v1770301900/range-front/parallax/background-image.webp') no-repeat fixed bottom center;
-    width: 100%;
-    height: 96rem;
+  background-size: cover;
+  width: 100%;
+  height: 96rem;
 `;
 
 const Parallax = () => (
